fa1fe4a4e58ed2de5e06981bcde1d584dec01704
vinay bhat <bhatvinay74@gmail.com>
2024-04-17 16:45:42 +0530

    memo file created

---
+++ Memo.js
@@ -0,0 +1,31 @@
+import { memo, useState } from 'react';
+
+export default function MyApp() {
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  return (
+    <>
+      <label>
+        Name{': '}
+        <input value={name} onChange={e => setName(e.target.value)} />
+      </label>
+      <label>
+        Address{': '}
+        <input value={address} onChange={e => setAddress(e.target.value)} />
+      </label>
+      <Greeting name={name} />
+      <Address address={address}/>
+    </>
+  );
+}
+
+const Greeting = memo(function Greeting({ name }) {
+  console.log("Greeting was rendered at", new Date().toLocaleTimeString());
+  return <h3>Hello{name && ', '}{name}!</h3>;
+});
+
+
+const Address= memo(function Address({ address }) {
+  console.log("Address was rendered at", new Date().toLocaleTimeString());
+  return <h3>Hello{address && ', '}{address}!</h3>;
+});
